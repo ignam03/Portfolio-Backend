7c2fe326579348788b115df148076177d5e2e409
ignam03 <ignafree97@gmail.com>
2022-08-01 03:26:56 -0300

    entidades de usuario y educacion

--- src/main/java/com/portfoliobackend/entity/Educacion.java
+++ src/main/java/com/portfoliobackend/entity/Educacion.java
@@ -1,5 +1,51 @@
 package com.portfoliobackend.entity;
 
-public class Educacion {
-    
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "eduacion")
+@Component
+@Getter
+@Setter
+public class Educacion implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long educacionId;
+    private String titulo;
+    private String nombreInstituacion;
+    private Integer anio;
+    private String descripcionEdu;
+    private int fechaEdu;
+
+    public Educacion() {
+    }
+
+    public Educacion(Long educacionId, String titulo, String nombreInstituacion, Integer anio, String descripcionEdu,
+            int fechaEdu) {
+        this.educacionId = educacionId;
+        this.titulo = titulo;
+        this.nombreInstituacion = nombreInstituacion;
+        this.anio = anio;
+        this.descripcionEdu = descripcionEdu;
+        this.fechaEdu = fechaEdu;
+    }
+
+    @Override
+    public String toString() {
+        return "Educacion [anios=" + anio + ", descripcionEdu=" + descripcionEdu + ", educacionId=" + educacionId
+                + ", fechaEdu=" + fechaEdu + ", nombreInstituacion=" + nombreInstituacion + ", titulo=" + titulo + "]";
+    }
+
 }
--- src/main/java/com/portfoliobackend/entity/Usuario.java
+++ src/main/java/com/portfoliobackend/entity/Usuario.java
@@ -1,5 +1,56 @@
 package com.portfoliobackend.entity;
 
-public class Usuario {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Component
+@Getter @Setter
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+    @Column(name = "apellido", nullable = false)
+    private String apellido;
+    @Column(name = "descripcion")
+    private String descripcion;
+    @Column(name = "titulo", nullable = false)
+    private String titulo;
+    @Column(name = "foto_perfil")
+    private String fotoPerfil;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long userId, String nombre, String apellido, String descripcion, String titulo, String fotoPerfil) {
+        this.userId = userId;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.descripcion = descripcion;
+        this.titulo = titulo;
+        this.fotoPerfil = fotoPerfil;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [apellido=" + apellido + ", descripcion=" + descripcion + ", fotoPerfil=" + fotoPerfil
+                + ", nombre=" + nombre + ", titulo=" + titulo + ", userId=" + userId + "]";
+    }
+
     
 }
