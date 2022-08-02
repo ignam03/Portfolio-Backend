4f643e5674fb7b93345fac6389bd21dab88165b4
ignam03 <ignafree97@gmail.com>
2022-08-01 21:08:25 -0300

    arreglo de cors

--- src/main/java/com/portfoliobackend/PortoflioApplication.java
+++ src/main/java/com/portfoliobackend/PortoflioApplication.java
@@ -2,6 +2,9 @@ package com.portfoliobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortoflioApplication {
@@ -10,4 +13,14 @@ public class PortoflioApplication {
 		SpringApplication.run(PortoflioApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("PUT", "DELETE",
+						"GET", "POST");
+			}
+		};
+	}
 }
