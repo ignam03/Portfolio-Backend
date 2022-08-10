41b9c048f8602e8a63ee7a42aa36c3a86c63ccfc
ignam03 <ignafree97@gmail.com>
2022-08-09 23:11:23 -0300

    entidades rol user y enum de roles en caso de necesitarlos

---
+++ src/main/java/com/portfoliobackend/security/entity/Rol.java
@@ -0,0 +1,38 @@
+package com.portfoliobackend.security.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.portfoliobackend.security.entity.enums.ERol;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERol name;
+
+    public Rol() {
+    }
+
+    public Rol(ERol name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/portfoliobackend/security/entity/enums/ERol.java
@@ -0,0 +1,7 @@
+package com.portfoliobackend.security.entity.enums;
+
+public enum ERol {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
