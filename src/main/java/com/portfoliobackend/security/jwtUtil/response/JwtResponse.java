805e07a62d06dbb78502fc0612fde4f482be89a2
ignam03 <ignafree97@gmail.com>
2022-08-09 23:12:26 -0300

    login request y jwtreponse

---
+++ src/main/java/com/portfoliobackend/security/jwtUtil/response/JwtResponse.java
@@ -0,0 +1,28 @@
+package com.portfoliobackend.security.jwtUtil.response;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JwtResponse {
+
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private List<String> roles;
+
+    public JwtResponse(String accessToken, String type, Long id, String username, String email, List<String> roles) {
+        this.token = accessToken;
+        this.type = type;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+}
