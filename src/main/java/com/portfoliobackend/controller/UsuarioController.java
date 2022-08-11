672434658dc692de779536b4761d37efe439abb2
ignam03 <ignafree97@gmail.com>
2022-08-11 18:41:55 -0300

    arreglos para rescontroller

--- src/main/java/com/portfoliobackend/controller/UsuarioController.java
+++ src/main/java/com/portfoliobackend/controller/UsuarioController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.portfoliobackend.entity.Usuario;
 import com.portfoliobackend.service.IUsuarioService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/portfolio")
-@Controller
 public class UsuarioController {
 
     @Autowired
